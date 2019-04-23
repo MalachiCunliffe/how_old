0081f80c79a0c42b6ae0e708ff5205511e61cc0e
Malachi Cunliffe <mal.cunliffe@gmail.com>
2019-04-23 18:33:11 +1200

    Changed Text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import './Header.css'
 export default function Header() {
     return (
         <div className="header">
-            <h1 className='headerText'>How Old? 🤔</h1>
+            <h1 className='headerText'>How old are you ?, upload a photo and we'll guess!! 🤔</h1>
         </div>
     )
 }
